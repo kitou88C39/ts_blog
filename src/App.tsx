fdb9a4d19f5dd3de167b3326e7d2e186b54f7661
kitou88C39 <kanfutrooper@icloud.com>
2022-09-03 20:45:54 +0900

    fifth commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,33 @@ import Login from './components/Login';
 import AddUser from './features/AddUser';
 import EditUser from './features/EditUser';
 import UserList from './features/UserList';
-//import { Amplify } from "aws-amplify";
-// import { withAuthenticator } from "@aws-amplify/ui-react";
-// import @aws-amplify/ui-react/style.css;
-// import awsExports from "./aws-exports";
-// Amplify.configure(awsExports);
-
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
   return (
+    <>
     <div className='flex flex-col h-full bg-white shadow-lg max-h-16'>
       <Header />
+      </div>
+     <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+        {user? (
+          <>
+          <h1>Login {user.username}</h1>
+          </>
+        ):( 
+        <h1>Log out</h1>
+           )}
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  
       <div className='container max-w-5xl px-2 pt-10 mx-auto md:pt-32'>
         <Routes>
           <Route path='/' element={<UserList />} />
@@ -23,7 +39,8 @@ function App() {
           <Route path='/login' element={<Login />} />
         </Routes>
       </div>
-    </div>
+    
+    </>
   );
 }
 
